b448fd5cfd79eef27a5488ee1ff5bba1f261ff8c
mihaeltensek69-netizen <mihaeltensek69@gmail.com>
2025-08-20 14:10:14 +0200

    Delete components/ui directory

--- components/ui/Input.jsx
+++
@@ -1,6 +0,0 @@
-export function Input(props) {
-  return <input {...props} className={`w-full border border-slate-300 rounded-2xl px-3 py-2 outline-none focus:ring focus:ring-slate-200 ${props.className||""}`} />;
-}
-export function Textarea(props) {
-  return <textarea {...props} className={`w-full border border-slate-300 rounded-2xl px-3 py-2 outline-none focus:ring focus:ring-slate-200 ${props.className||""}`} />;
-}
